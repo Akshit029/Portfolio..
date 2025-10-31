bfc0ad74851cb02fa21fce83f61db0d3118bf611
Akshit chadgal <akshitchadgal016@gmail.com>
2025-10-31 21:31:57 +0530

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,14 +7,11 @@ interface RepoStats {
 }
 
 const Footer = async () => {
-    const repoStats = await fetch(
-        'https://api.github.com/repos/tajmirul/portfolio-2.0',
-        {
-            next: {
-                revalidate: 60 * 60, // 1 hour
-            },
+    const repoStats = await fetch('', {
+        next: {
+            revalidate: 60 * 60, // 1 hour
         },
-    );
+    });
 
     const { stargazers_count, forks_count } =
         (await repoStats.json()) as RepoStats;
@@ -32,7 +29,7 @@ const Footer = async () => {
 
                 {/* <div className="">
                     <a
-                        href="https://github.com/Tajmirul/portfolio-2.0"
+                        href=""
                         target="_blank"
                         className="leading-none text-muted-foreground hover:underline hover:text-white"
                     >
@@ -46,9 +43,9 @@ const Footer = async () => {
                         </div>
                     </a> 
 
-                    {/* Note: If you are not Tajmirul, use this copyright message instead */}
+                    {/* Note: If you are not , use this copyright message instead */}
                 {/* <a href='https://www.me.toinfinite.dev/' className="leading-none text-muted-foreground hover:underline hover:text-white">
-                        Design & built by Tajmirul Islam <br />
+                        Design & built by Islam <br />
                         Revised by YOUR NAME
 
                         <div className="flex items-center justify-center gap-5 pt-1">
